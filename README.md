e17e8abfb6c804ab2144c7061588664c73306f39
fedejinich <jinichfede@gmail.com>
2023-02-19 13:14:34 -0300

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# chip-8-rs
+
+This is a Rust implementation of the Chip-8 virtual machine, a simple 8-bit computer designed in the 1970s for creating simple video games and educational programs.
+
+## Testing
+To run the tests, execute the following command:
+
+```sh
+cargo test
+```
+This will run all of the tests in the project, including unit tests and integration tests. 
+
---
+++ src/chip_8.rs
@@ -0,0 +1,42 @@
+const PROGRAM_START_ADDRESS: u16 =0x200;
+
+const MEM_SIZE: usize = 4096;
+const NUM_REGS: usize = 16;
+const STACK_SIZE: usize = 16;
+
+const SCREEN_WIDTH: usize = 64;
+const SCREEN_HEIGTH: usize = 32;
+
+pub struct Chip8 {
+    v_reg: [u8; NUM_REGS],
+    i: u16,
+    pc: u16,
+    memory: [u8; MEM_SIZE],
+    stack: [u16; STACK_SIZE],
+    stack_pointer: u8,
+    display: [bool; SCREEN_WIDTH * SCREEN_HEIGTH],
+    delay_timer: u8,
+    sound_timer: u8,
+}
+
+impl Default for Chip8 {
+    fn default() -> Self {
+        Chip8 {
+            v_reg: [0; NUM_REGS],
+            i: 0,
+            pc: PROGRAM_START_ADDRESS,
+            memory: [0; MEM_SIZE],
+            stack: [0; STACK_SIZE],
+            stack_pointer: 0,
+            display: [false; SCREEN_WIDTH * SCREEN_HEIGTH],
+            delay_timer: 0,
+            sound_timer: 0,
+        }
+    }
+}
+
+impl Chip8 {
+    pub fn load_program(self: Chip8) {
+        todo!()
+    }
+}
